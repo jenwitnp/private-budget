1d9f9ecab0447af71b26f0df9209b7b445dd8ac6
Budget Project <test@example.com>
2026-03-19 15:52:02 +0700

    add maintainance mode

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,10 @@ import { AppProviders } from "@/lib/providers/AppProviders";
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
+  if (process.env.NEXT_PUBLIC_MAINTENANCE === "true") {
+    return <>Maintain</>; // แสดงหน้าปิดปรับปรุงแทนทุกหน้า
+  }
+  // return <Component {...pageProps} />;
   return (
     <AppProviders>
       <Component {...pageProps} />
